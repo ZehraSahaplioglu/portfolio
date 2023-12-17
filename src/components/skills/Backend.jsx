d68212c7f60f2e2accaf0ff6df6e1b7c02eeac8a
55279283562 <sahapliogluzehra@gmail.com>
2023-12-17 21:15:21 +0300

    projenin skills kısmı tamamlandı

---
+++ src/components/skills/Backend.jsx
@@ -0,0 +1,76 @@
+import React from 'react'
+
+const Backend = () => {
+  return (
+    <div className="skills__content">
+        <h3 className="skills__title">Backend Developer</h3>
+
+        <div className="skills__box">
+            <div className="skills__group">
+
+                <div className="skills__data">
+                    <i class='bx bx-badge-check'></i>
+                    
+                    <div>
+                        <h3 className="skills__name">PostgreSql</h3>
+                        <span className="skills__level">Başlangıç</span>
+                    </div>
+                </div>
+
+                <div className="skills__data">
+                    <i class='bx bx-badge-check'></i>
+                    
+                    <div>
+                        <h3 className="skills__name">Sql Server</h3>
+                        <span className="skills__level">Başlangıç</span>
+                    </div>
+                </div>
+
+                <div className="skills__data">
+                    <i class='bx bx-badge-check'></i>
+                    
+                    <div>
+                        <h3 className="skills__name">Java</h3>
+                        <span className="skills__level">Orta Düzey</span>
+                    </div>
+                </div>
+
+            </div>
+
+            <div className="skills__group">
+
+                <div className="skills__data">
+                    <i class='bx bx-badge-check'></i>
+                    
+                    <div>
+                        <h3 className="skills__name">Node JS</h3>
+                        <span className="skills__level">Başlangıç</span>
+                    </div>
+                </div>
+
+                <div className="skills__data">
+                    <i class='bx bx-badge-check'></i>
+                    
+                    <div>
+                        <h3 className="skills__name">Android Studio</h3>
+                        <span className="skills__level">Orta Düzey</span>
+                    </div>
+                </div>
+
+                <div className="skills__data">
+                    <i class='bx bx-badge-check'></i>
+                    
+                    <div>
+                        <h3 className="skills__name">Asp.Net Core Mvc</h3>
+                        <span className="skills__level">Orta Düzey</span>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default Backend
---
+++ src/components/skills/Skills.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import "./skills.css"
+import Frontend from './Frontend';
+import Backend from './Backend';
+
+const Skills = () => {
+  return (
+    <section className="skills section" id="skills">
+        <h2 className="section__title">Yetenekler</h2>
+        <span className="section__subtitle">Teknik Seviyem</span>
+
+        <div className="skills__container container grid">
+            <Frontend />
+            <Backend />
+        </div>
+    </section>
+  )
+}
+
+export default Skills
