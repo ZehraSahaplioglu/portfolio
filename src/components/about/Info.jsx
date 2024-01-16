69e076cdfec3da267de98587fdfecf9d895df9f0
ZehraSahaplioglu <99864727+ZehraSahaplioglu@users.noreply.github.com>
2024-01-16 18:19:16 +0300

    Update Info.jsx

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -12,7 +12,7 @@ const Info = () => {
         <div className="about__box">
             <i class='bx bx-briefcase about__icon'></i>
             <h3 className="about__title">Tamamlanmış Projeler</h3>
-            <span className="about__subtitle">3 + Proje</span>
+            <span className="about__subtitle">4 + Proje</span>
         </div>
 
         <div className="about__box">
